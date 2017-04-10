ab5f598825617f502e3a92f8253ff84ff911fefe
Edvard <edvard.jo@gmail.com>
2017-04-10 21:12:02 +0200

    Plot data konstruktor

--- eclipse-workspace/furuta-project/src/PlotData.java
+++ eclipse-workspace/furuta-project/src/PlotData.java
@@ -2,6 +2,16 @@ public class PlotData implements Cloneable {
 	double ref, y;
 	double x; // holds the current time 
 	
+	public PlotData(double x, double yref, double y) {
+		this.ref = yref;
+		this.x = x;
+		this.y = y;
+	}
+	
+	public PlotData() {
+		
+	}
+	
 	public Object clone() {
 		try {
 			return super.clone();
