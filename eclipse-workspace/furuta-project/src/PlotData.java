2aad6cd75615b08f7dba1ad308539f298abf2a19
Edvard <edvard.jo@gmail.com>
2017-04-10 18:02:05 +0200

    Lade till LQParameters och Plotdata

---
+++ eclipse-workspace/furuta-project/src/PlotData.java
@@ -0,0 +1,12 @@
+public class PlotData implements Cloneable {
+	double ref, y;
+	double x; // holds the current time 
+	
+	public Object clone() {
+		try {
+			return super.clone();
+		} catch (Exception e) {
+			return null;
+		}
+	}
+}
